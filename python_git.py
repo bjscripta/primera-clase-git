4678b5c40c645c0a4ba441ffdb0c10a7537d5c10
benja <benj.candia@duocuc.cl>
2024-05-29 09:22:25 -0400

    creacion de diccionario

---
+++ python_git.py
@@ -0,0 +1,9 @@
+#Ejercicio diccionario de hoy: usuarios:
+
+nario = {
+    "identificador": 1,
+    "Nombre": "Marriott",
+    "Direccion": "Avenida bernando o'higgins 1833",
+    "Funcionando": True,
+    "valor_hab": 120000
+}
