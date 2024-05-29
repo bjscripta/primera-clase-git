b590804cfa7965ca53d80ad640f71041143d25d4
benja <benj.candia@duocuc.cl>
2024-05-29 11:05:51 -0400

    opc 2 y 3

--- python_git.py
+++ python_git.py
@@ -11,8 +11,10 @@ nario = {
     "Funcionando": True,
     "valor_hab": 120000
 }
-usuarios = []
+listuser = []
+listpass = []
 while True:
+    os.system("cls")
     print("""
           1. Inicio de sesion
           2. Registrar usuario
@@ -27,13 +29,52 @@ while True:
                 print("Error, opcion no disponible")
         except:
             print("Error, solo se permiten numeros enteros!")
-        
+
+    os.system("cls")   
     if opc==1:
         pass
     elif opc==2:
-        pass
+        print("-"*20)
+        print("Registrar usuario")
+        print("-"*20)
+        while True:
+            usuario = input("Ingrese nombre de usuario: ")
+            if len(usuario) > 3:
+                break
+            else:
+                print("Error, el usuario debe contener al menos 3 caracteres!")
+        while True:
+            senia = input("Ingrese contraseña: ")
+            if len(senia) > 3:
+                break
+            else:
+                print("Error, la contraseña debe contener al menos 3 caracteres")
+        print("-"*20)
+        print("Usuario registrado!")
+        print("Presione una tecla para continuar")
+        print("-"*20)
+        msvcrt.getch()
+
+        listuser.append(usuario)
+        listpass.append(senia)
+
     elif opc==3:
-        pass
+        print("-"*20)
+        print("Eliminar usuario")
+        print("-"*20)
+        while True:
+            elim = input("Ingrese usuario a eliminar: ")
+            if elim in listuser:
+                listuser.remove(usuario)
+                print("-"*20)
+                print("Usuario eiminado con exito!")
+                print("Presione tecla para continuar")
+                print("-"*20)
+                msvcrt.getch()
+                break
+            else:
+                print("Usuario no existente!")
+
     else:
         print("Hasta luego!")
         break
